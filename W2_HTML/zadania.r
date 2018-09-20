2398d585519fdd1a384f46f6386e081479285326
Statystyka Bio <43376719+statSGGW@users.noreply.github.com>
2018-09-20 08:55:10 +0200

    Add files via upload

--- W2_HTML/zadania.r
+++ W2_HTML/zadania.r
@@ -7,9 +7,10 @@ head(daneSoc)
 
 attach(daneSoc)
 
-install.packages(c("stats", "e1071", "car"))
+install.packages(c("stats", "e1071", "psych", "car"))
 library("stats")
 library("e1071")
+library("psych")
 library("car")
 
 # zakres
@@ -34,7 +35,7 @@ harmonic.mean(wiek)
 median(wiek)
 
 # moda
-moda(wiek)
+modes::mode(wiek)
 
 # kwantyle
 quantile(wiek, c(0.1,0.25,0.5,0.75,0.9))
@@ -46,7 +47,7 @@ mad(wiek)
 sd(wiek)
 
 # kurtoza
-kurtosis(wiek)
+kurtosi(wiek)
 
 # skośność
 skewness(wiek)
@@ -62,7 +63,7 @@ table(wyksztalcenie, praca)
 hist(wiek,5,main="Histogram zmiennej wiek", ylab="liczebności")
 rug(wiek, side=1,ticksize=0.03,col="red")
 
-h <- hist(wiek,5,main="Histogram zmiennej wiek", ylab="liczebności")
+h <- hist(wiek,30,main="Histogram zmiennej wiek", ylab="liczebności")
 rug(jitter(wiek, factor=2), side=1,ticksize=0.03,col="red")
 str(h)
 
@@ -76,10 +77,10 @@ tmpx = jitter(rep(1.3, length(wiek)), factor=3)
 points(tmpx, wiek, pch=16, col="lightgrey", cex=0.5)
 
 # wykres skrzypcowy
-violplot(wiek[plec=="mezczyzna"], wiek[plec=="kobieta"], wiek, names=c("wiek M", "wiek K", "wiek"))
+violinplot(wiek[plec=="mezczyzna"], wiek[plec=="kobieta"], wiek, names=c("wiek M", "wiek K", "wiek"))
 
 # wykres rozrzutu
-sp(cisnienie.skurczowe,cisnienie.rozkurczowe,groups=plec,smooth=FALSE, lwd=3, pch=c(20,21), cex=1.5)
+sp(cisnienie.skurczowe,cisnienie.rozkurczowe,groups=plec,smooth=FALSE, boxplots="xy", lwd=3, pch=c(20,21), cex=1.5)
 
 
 Zadania do samodzielnego wykonania
